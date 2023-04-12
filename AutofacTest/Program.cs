f83edf778d7473ea9f703c85bb4898d5c10de784
NightFish <NightFish.net@gmail.com>
2023-04-13 09:48:16 +1200

    Add boilerplate

---
+++ AutofacTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using AutofacTest;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(config =>
+    {
+        config.AddEnvironmentVariables();
+        config.AddUserSecrets<Program>();
+    })
+    .UseSerilog((context, config) =>
+    {
+        config.WriteTo.Console();
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services
+            .AddAutofac()
+            .AddHostedService<Startup>();
+    })
+    .Build()
+    .RunAsync();
---
+++ AutofacTest/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AutofacTest;
+
+public class Startup : IHostedService
+{
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<Startup> _logger;
+    
+    public Startup(IConfiguration configuration, ILogger<Startup> logger)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+    
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Hello!");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
